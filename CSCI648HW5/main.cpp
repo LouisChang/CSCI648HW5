086c6b63c30442dd32fdb7430e31af96e828d501
Sidi Chang <sidichang@Sidis-MacBook-Pro.local>
2015-03-06 12:52:35 -0500

    Initial Commit

---
+++ CSCI648HW5/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  CSCI648HW5
+//
+//  Created by Sidi Chang on 3/6/15.
+//  Copyright (c) 2015 Sidi Chang. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
